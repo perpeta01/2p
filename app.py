b1d899e78755601cb441d215f08d6b37335b5594
matheus <perpeta01@gmail.com>
2024-10-24 18:57:15 -0300

    Atualização do bot e correções

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ class MinesBot:
             options.add_argument("--start-maximized")
             # Simulação de User-Agent de dispositivo móvel
             options.add_argument("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1")
+            # Opções essenciais para rodar no Heroku
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+
             self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
             self.driver.get('https://blaze.com/pt/games/mines')
             print("Por favor, faça login manualmente.")
